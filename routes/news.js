5e1d9bac41398f2df730d486f93b11a36a28b87a
princeUA <prince92@ukr.net>
2016-11-17 06:12:47 +0200

    Update news.js

--- routes/news.js
+++ routes/news.js
@@ -2,7 +2,6 @@ var login = require('routes/login');
 var db = require('db.js');
 
 exports.get = function (req, res) {
-db.connection.query("CREATE TABLE `petrykdb`.`users` (`id` INT UNSIGNED NOT NULL AUTO_INCREMENT, `mail` VARCHAR(100) NOT NULL, `name` VARCHAR(100) NOT NULL, `password` VARCHAR(100) NOT NULL, `role` VARCHAR(45) NOT NULL DEFAULT 'user', PRIMARY KEY (`id`), UNIQUE INDEX `id_UNIQUE` (`id` ASC), UNIQUE INDEX `mail_UNIQUE` (`mail` ASC)) ENGINE = InnoDB  DEFAULT CHARACTER SET = utf8 COLLATE = utf8_unicode_ci");
     res.render('news');
 };
 

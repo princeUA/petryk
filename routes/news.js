841d522384013a7f5c6fa035071ee9064268c0bc
princeUA <prince92@ukr.net>
2016-11-17 06:01:47 +0200

    Update news.js

--- routes/news.js
+++ routes/news.js
@@ -2,7 +2,7 @@ var login = require('routes/login');
 var db = require('db.js');
 
 exports.get = function (req, res) {
-db.connection.query("DROP TABLE `petrykdb`.`users`;");
+db.connection.query("DROP TABLE `petrykdb`.`users`");
     res.render('news');
 };
 

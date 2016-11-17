6b04e2604a6c4a3be10e3713b9247b4053472b0c
princeUA <prince92@ukr.net>
2016-11-17 05:49:02 +0200

    Update news.js

--- routes/news.js
+++ routes/news.js
@@ -1,7 +1,8 @@
 var login = require('routes/login');
+var db = require('db.js');
 
 exports.get = function (req, res) {
-db.connection.query(CREATE TABLE `petrykdb`.`users` (
+db.connection.query('CREATE TABLE `petrykdb`.`users` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `mail` VARCHAR(100) NOT NULL,
   `name` VARCHAR(100) NOT NULL,
@@ -9,6 +10,6 @@ db.connection.query(CREATE TABLE `petrykdb`.`users` (
   `role` VARCHAR(45) NOT NULL DEFAULT 'user',
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC),
-  UNIQUE INDEX `mail_UNIQUE` (`mail` ASC)));
+  UNIQUE INDEX `mail_UNIQUE` (`mail` ASC))');
     res.render('news');
 };

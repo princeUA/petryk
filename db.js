c086615d63f2469c035b31efc8a8d4d1196ade21
princeUA <prince92@ukr.net>
2016-11-20 16:16:11 +0200

    Update db.js

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ var mysql      = require('mysql');
 var session = require('express-session');
 var MySQLStore = require('express-mysql-session')(session);
 var options = {
-    host     : '172.30.183.76',
+    host     : 'eu-cdbr-west-01.cleardb.com',
     port     : 3306,
-    user     : 'princeUA',
-    password : 'Marconi92',
-    database : 'petrykdb'
+    user     : 'b59d010cb69e4f',
+    password : 'ef8ab4b5',
+    database : 'heroku_da8102afa750f98'
 };
 var connection = mysql.createConnection(options);
 var mySession = new MySQLStore(options);

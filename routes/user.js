e2b7ae9c7812c304987cb0e92f2733669035e94b
princeUA <prince92@ukr.net>
2016-12-08 07:43:34 +0200

    1

--- routes/user.js
+++ routes/user.js
@@ -1,20 +1,29 @@
-var login = require('routes/login');
-var HttpError = require('error').HttpError;
-var db = require('db');
+var login = require("routes/login");
+var HttpError = require("error").HttpError;
+var db = require("db");
 
 exports.get = function (req, res, next) {
-    db.connection.query('SELECT * FROM users WHERE id = ?', req.params.id, function(err, user){
+    db.connection.query("SELECT * FROM users WHERE id = ?", req.params.id, function(err, user){
         if(err) {
             next(err);
         } else {
-            res.render('user', {user: user});
+            res.render("user", {user: user});
         }
     });
 };
 
 exports.post = function(req, res, next) {
-    if(req.body.login == '') {
+    console.log(req.body);
+    if(req.body.login == "") {
         login.login(req, res, next);
+    } else if(req.body.confRole == ""){
+        console.log('111');
+        db.connection.query('UPDATE users SET role = ? WHERE id = ?', [req.body.role, req.params.id], function(err){
+            if(err) {
+                next(err);
+            } else {
+                res.redirect("#");
+            }
+        })
     }
-
 };

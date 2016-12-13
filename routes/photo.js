cc3ac806ed7d5bb4ae50e8c7a486b724944721f5
princeUA <prince92@ukr.net>
2016-12-13 22:02:32 +0200

    1

--- routes/photo.js
+++ routes/photo.js
@@ -20,26 +20,32 @@ exports.post = function(req, res, next) {
     else if(req.body.action == "editPhotos"){
         if(!req.role || req.role != "admin"){
             res.end("403");
-        }
-        db.connection.query("UPDATE albums SET photos = ? WHERE id = ?", [req.body.photos, req.params.id], function(err, photos){
-            console.log(req.body.photos);
-            console.log(req.params.id);
-            console.log(photos);
-            if(err){
-                next(new HttpError(err));
-            } else {
-                res.end("done");
-            }
-        })
+        } else {
+            db.connection.query("UPDATE albums SET photos = ? WHERE id = ?", [req.body.photos, req.params.id], function(err, photos){
+                console.log(req.body.photos);
+                console.log(req.params.id);
+                console.log(photos);
+                if(err){
+                    next(new HttpError(err));
+                } else {
+                    res.end("done");
+                }
+            });
+        }        
     }
     else if(req.body.del == '') {
-        db.connection.query('DELETE FROM news WHERE id = ?', req.params.id, function(err){
-            if(err){
-                next(new HttpError(err));
-            } else {
-                res.redirect('/photos');
-            }
-        });
+        if(!req.role || req.role != "admin"){
+            res.end("403");
+        } else {
+            db.connection.query('DELETE FROM albums WHERE id = ?', req.params.id, function(err){
+                if(err){
+                    next(new HttpError(err));
+                } else {
+                    res.redirect('/photos');
+                }
+            });
+        }
+        
     }
 
 };

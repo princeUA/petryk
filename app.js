e46ad51dd78f7c886783600f5bd22b85f1ed8345
princeUA <prince92@ukr.net>
2016-12-20 07:16:24 +0200

    1

--- app.js
+++ app.js
@@ -2,39 +2,47 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var config = require('config');
-var log = require('libs/log')(module);
+var logger = require('morgan');
+//var log = require('libs/log')(module);
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var errorHandler = require('errorhandler');
 var db = require('db.js');
 var HttpError = require('error').HttpError;
-
+var favicon = require('serve-favicon');
 
 var app = express();
-
+// all environments
 app.engine('ejs', require('ejs-locals'));
+app.set('port', process.env.PORT || 3000)
 app.set('views', path.join(__dirname, 'templates'));
 app.set('view engine', 'ejs');
+app.use(favicon(path.join(__dirname, '/public/favicon.ico')));
 
-app.use(express.favicon());
 if (app.get('env') == 'development') {
-    app.use(express.logger('dev'));
+    app.use(logger('dev'));
 } else {
-    app.use(express.logger('default'));
+    app.use(logger('default'));
 }
-app.use(express.json());
-app.use(express.urlencoded()); // req.body
 
-app.use(express.cookieParser()); //req.cookies
-
-app.use(express.session({
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
     secret: config.get('session:secret'),
     cookie: config.get('session:cookie'),
     store: db.mySession
 }));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // req.body
+
+app.use(cookieParser()); //req.cookies
+
 app.use(require('middleware/loadUser'));
 
 app.use(require('middleware/sendHttpError'));
-
-app.use(app.router);
 
 require('routes')(app);
 
@@ -48,7 +56,7 @@ app.use(function (err, req, res, next) {
         res.sendHttpError(err);
     } else {
         if (app.get('env') == 'develop') {
-            var errorHandler = express.errorHandler();
+            var errorHandler = errorHandler();
             errorHandler(err, req, res, next);
         } else {
             log.error(err);
@@ -57,7 +65,6 @@ app.use(function (err, req, res, next) {
         }
     }
 });
-var port = process.env.PORT || 3000;
-http.createServer(app).listen(port, function () {
-    log.info('Express server listening on port ' + port);
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });

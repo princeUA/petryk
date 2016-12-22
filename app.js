471853fac3237cd2989b1ceecdafe275bfe06b70
princeUA <prince92@ukr.net>
2016-12-22 07:21:53 +0200

    1

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var favicon = require('serve-favicon');
 var app = express();
 // all environments
 app.engine('ejs', require('ejs-locals'));
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 3005)
 app.set('views', path.join(__dirname, 'templates'));
 app.set('view engine', 'ejs');
 app.use(favicon(path.join(__dirname, '/public/favicon.ico')));
